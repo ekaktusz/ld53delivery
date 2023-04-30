06803cab929c2881cfcfbe949369c3f2a4e91cb3
Csaba Ekart <csaba.ekart@gmail.com>
2023-04-30 20:11:09 +0200

    button and shop improvements

--- include/button.lua
+++ include/button.lua
@@ -5,21 +5,26 @@ function button_new(_x,_y,_w,_h,_text)
         w=_w,
         h=_h,
         text=_text,
-        on_click = nil
+        on_click = nil,
+        click_c = 3,
+        c = 0
     }
     return button
 end
 
 function button_draw(button)
     rectfill(button.x+cam.x, button.y+cam.y, button.x+cam.x+button.w, button.y+cam.y+button.h, 7)
-    rectfill(button.x+cam.x+1, button.y+cam.y+1, button.x+cam.x+button.w-1, button.y+cam.y+button.h-1, 0)
+    rectfill(button.x+cam.x+1, button.y+cam.y+1, button.x+cam.x+button.w-1, button.y+cam.y+button.h-1, button.c)
     print("buy",button.x+cam.x+2, button.y+cam.y+2,7)
 end
 
 function button_update(button)
-    if mouse_in_area(button.x+cam.x, button.y+cam.y, button.w, button.h) then
+    if mouse_in_area(button.x+cam.x, button.y+cam.y, button.w, button.h) and mouse.click then
+        button.c = button.click_c
         if button.on_click != nil then
             button.on_click()
         end
+    else
+        button.c = 0
     end
 end
--- include/dealer.lua
+++ include/dealer.lua
@@ -13,9 +13,9 @@ function dealer_init()
         h = 56
     }
     dealer.in_shop = true
-    dealer.buy_btn1 = button_new(shop.x + flr(shop.w/3)*2, shop.y,                 flr(shop.w/3), flr(shop.h/3), "buy")
-    dealer.buy_btn2 = button_new(shop.x + flr(shop.w/3)*2, shop.y+flr(shop.h/3),   flr(shop.w/3), flr(shop.h/3), "buy")
-    dealer.buy_btn3 = button_new(shop.x + flr(shop.w/3)*2, shop.y+flr(shop.h/3)*2, flr(shop.w/3), flr(shop.h/3), "buy")
+    dealer.buy_btn1 = button_new(shop.x + flr(shop.w/3)*2, shop.y + 3,                 flr(shop.w/3)-1, 12, "buy")
+    dealer.buy_btn2 = button_new(shop.x + flr(shop.w/3)*2, shop.y+flr(shop.h/3) + 3,   flr(shop.w/3)-1, 12, "buy")
+    dealer.buy_btn3 = button_new(shop.x + flr(shop.w/3)*2, shop.y+flr(shop.h/3)*2 + 3, flr(shop.w/3)-1, 12, "buy")
 end
 
 
@@ -29,17 +29,28 @@ end
 
 local function dealer_draw_shop()
     rectfill(shop.x, shop.y, shop.x+shop.w, shop.y+shop.h, 7)
-        rectfill(shop.x+1, shop.y+1, shop.x+shop.w-1, shop.y+shop.h-1, 0)
-        
-        local offset_x = 4
-        local offset_y = 4
-        sspr(48, 0, 16, 16, shop.x + offset_x, shop.y + offset_y)
-        sspr(96, 0, 16, 16, shop.x+shop.w/3 + offset_x, shop.y + offset_y)
-        sspr(80, 0, 16, 16, shop.x+shop.w/3*2 + offset_x, shop.y + offset_y)
+    rectfill(shop.x+1, shop.y+1, shop.x+shop.w-1, shop.y+shop.h-1, 0)
+    
+    local offset_x = 2
+    local offset_y = 1
+    
+    sspr(48, 0, 16, 16, shop.x + offset_x, shop.y + offset_y) -- marijuana
+    sspr(96, 0, 16, 16, shop.x + offset_x, shop.y + offset_y + flr(shop.h/3)) -- ecstasy
+    sspr(80, 0, 16, 16, shop.x + offset_x, shop.y + offset_y + flr(shop.h/3*2)) -- cocaine
 
-        button_draw(dealer.buy_btn1)
-        button_draw(dealer.buy_btn2)
-        button_draw(dealer.buy_btn3)
+    offset_y = 2
+
+    print("weed", shop.x + flr(shop.w/3),  shop.y + offset_y, 7)
+    print("molly", shop.x + flr(shop.w/3),  shop.y + offset_y + flr(shop.h/3),7)
+    print("coke",  shop.x + flr(shop.w/3), shop.y + offset_y + flr(shop.h/3)*2,7)
+
+    print("$10", shop.x + flr(shop.w/3),  shop.y + offset_y + 8, 7)
+    print("$15", shop.x + flr(shop.w/3),  shop.y + offset_y + 8 + flr(shop.h/3),7)
+    print("$20",  shop.x + flr(shop.w/3), shop.y + offset_y + 8 + flr(shop.h/3)*2,7)
+
+    button_draw(dealer.buy_btn1)
+    button_draw(dealer.buy_btn2)
+    button_draw(dealer.buy_btn3)
 end
 
 function dealer_draw()
